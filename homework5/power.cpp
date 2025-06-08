e2fb900d1915ab706400d918f220f733f84e8572
over-over-STEAK <ljiawei255@gmail.com>
2025-06-08 15:23:44 +0800

    Create power.cpp

---
+++ homework5/power.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int mul3(int a, int b, int c) {
+    return a * b * c;
+}
+
+int main() {
+    cout << "result: " << mul3(5, 3, 6) << endl;
+    return 0;
+}
