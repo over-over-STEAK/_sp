48a520f8ff4c27b28fbc8140598d3c26f0b7e82a
over-over-STEAK <ljiawei255@gmail.com>
2025-06-08 15:29:02 +0800

    Update power.cpp

--- homework5/power.cpp
+++ homework5/power.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-extern int power(int base, int exp);
+int power(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= base;
+    }
+    return result;
+}
 
 int main() {
     int base = 3, exp = 2;
@@ -8,4 +14,3 @@ int main() {
     printf("%d^%d = %d\n", base, exp, result);
     return 0;
 }
-
