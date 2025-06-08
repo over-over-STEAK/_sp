d1a05aed0c625e85a7948aa8a288aadafe9263d0
over-over-STEAK <ljiawei255@gmail.com>
2025-06-08 15:29:22 +0800

    Update power.s

--- homework5/power.s
+++ homework5/power.s
@@ -1,28 +1,49 @@
-	.file	"power.s"
+	.file	"power.cpp"
 	.text
 	.globl	power
 	.type	power, @function
 power:
 	pushl	%ebp
 	movl	%esp, %ebp
-	subl	$16, %esp         # 建立堆疊空間
-
-	movl	$1, -4(%ebp)      # int result = 1
-	movl	$0, -8(%ebp)      # int i = 0
-
-.Lloop:
-	movl	-8(%ebp), %eax    # eax = i
-	cmpl	12(%ebp), %eax    # 比較 i < exp
-	jge	.Lend              # 若 i >= exp 跳到結束
-
-	movl	-4(%ebp), %eax    # eax = result
-	imull	8(%ebp), %eax     # eax *= base
-	movl	%eax, -4(%ebp)    # result = eax
-
-	addl	$1, -8(%ebp)      # i++
-	jmp	.Lloop
-
-.Lend:
-	movl	-4(%ebp), %eax    # return result
+	subl	$16, %esp
+	movl	$1, -4(%ebp)
+	movl	$0, -8(%ebp)
+.L2:
+	movl	-8(%ebp), %eax
+	cmpl	12(%ebp), %eax
+	jge	.L4
+	movl	-4(%ebp), %eax
+	imull	8(%ebp), %eax
+	movl	%eax, -4(%ebp)
+	addl	$1, -8(%ebp)
+	jmp	.L2
+.L4:
+	movl	-4(%ebp), %eax
 	leave
 	ret
+	.size	power, .-power
+	.globl	main
+	.type	main, @function
+main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$32, %esp
+	movl	$3, -4(%ebp)
+	movl	$2, -8(%ebp)
+	movl	-4(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	-8(%ebp), %eax
+	movl	%eax, (%esp)
+	call	power
+	movl	%eax, -12(%ebp)
+	movl	-4(%ebp), %eax
+	movl	%eax, 8(%esp)
+	movl	-8(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	-12(%ebp), %eax
+	movl	%eax, (%esp)
+	call	printf
+	movl	$0, %eax
+	leave
+	ret
+	.size	main, .-main
