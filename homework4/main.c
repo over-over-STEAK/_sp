ea2d45244bb170d3f100eeb0a560695ccd0cbdfe
over-over-STEAK <ljiawei255@gmail.com>
2025-06-08 15:16:02 +0800

    Update main.c

--- homework4/main.c
+++ homework4/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-// mult3 是在外部組合語言檔定義的
-int mult3(int a, int b, int c);
+int mult3(int a, int b, int c); // 聲明由 mult.s 提供的函數
 
 int main() {
-    printf("mult3(3,2,5)=%d\n", mult3(3, 2, 5));
+    printf("mult3(3, 2, 5) = %d\n", mult3(3, 2, 5));
     return 0;
 }
