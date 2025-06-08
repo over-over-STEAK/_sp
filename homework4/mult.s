7b3ccca2c88fac19a7b553209f5601ae81651b50
over-over-STEAK <ljiawei255@gmail.com>
2025-06-08 15:15:09 +0800

    Update mult.s

--- homework4/mult.s
+++ homework4/mult.s
@@ -10,10 +10,10 @@
 # ----------------------------------------------------------------------------------------
 
         .global mult3
-
         .text
 mult3:
-        mov     %rdi, %rax                # result (rax) initially holds x
-        imul   %rsi, %rax                # if so, set result to y
-        imul   %rdx, %rax                # if so, set result to z
-        ret                               # the max will be in eax
+        mov     %rdi, %rax        # rax = a
+        imul    %rsi, %rax        # rax *= b
+        imul    %rdx, %rax        # rax *= c
+        ret                       # return rax
+
